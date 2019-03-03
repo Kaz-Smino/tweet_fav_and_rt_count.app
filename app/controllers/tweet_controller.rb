38e757438cc3ddc9d7c5e5852f9559ed895ca36b
suminofu0829 <kazuya19840829@yahoo.co.jp>
2019-03-03 15:00:46 +0900

    ランキングの件数を30に変更

--- app/controllers/tweet_controller.rb
+++ app/controllers/tweet_controller.rb
@@ -1,6 +1,6 @@
 class TweetController < ApplicationController
   def index
-    @tweets = Tweet.order(tweet_point: :desc).limit(10)
+    @tweets = Tweet.order(tweet_point: :desc).limit(30)
     # 今日から1週間前の日付を取得する
     # Tweetの中で、1週間
   end
