91be93a6ea7816062aca778f706a87d2b9350cf5
suminofu0829 <kazuya19840829@yahoo.co.jp>
2019-03-03 19:08:17 +0900

    pageネーション機能を実装

--- app/controllers/tweet_controller.rb
+++ app/controllers/tweet_controller.rb
@@ -1,7 +1,10 @@
 class TweetController < ApplicationController
   def index
     @tweets = Tweet.order(tweet_point: :desc).limit(100)
+    @tweets = Tweet.page(params[:page])
     # 今日から1週間前の日付を取得する
     # Tweetの中で、1週間
   end
+  app/views/tweet/index.html.erb
+  <%= paginate @tweets %>
 end
