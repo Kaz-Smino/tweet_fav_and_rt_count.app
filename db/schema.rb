5b12524491ed8b0acae4c1989dfda61a4c83acc3
suminofu0829 <kazuya19840829@yahoo.co.jp>
2019-03-03 18:07:41 +0900

    ユーザーのプロフィール画面URLを格納するカラム：urlを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_03_083212) do
+ActiveRecord::Schema.define(version: 2019_03_03_090626) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2019_03_03_083212) do
     t.integer "tweet_point", default: 0, null: false
     t.string "user_image", default: "", null: false
     t.string "name"
+    t.string "url"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
