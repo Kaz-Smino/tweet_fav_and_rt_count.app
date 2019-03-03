763f395345d60c830be404df50eb37bbab2287c0
suminofu0829 <kazuya19840829@yahoo.co.jp>
2019-03-03 19:30:29 +0900

    表示されるランキングがおかしいので修正中

--- app/controllers/tweet_controller.rb
+++ app/controllers/tweet_controller.rb
@@ -2,8 +2,8 @@ class TweetController < ApplicationController
   PER = 50
 
   def index
-    @tweets = Tweet.order(tweet_point: :desc).limit(100)
-    @tweets = Tweet.page(params[:page]).per(PER)
+    @tweets = Tweet.order(tweet_point: :desc).limit(100).page(params[:page]).per(PER)
+    # @tweets = Tweet.page(params[:page]).per(PER)
     # 今日から1週間前の日付を取得する
     # Tweetの中で、
   end
