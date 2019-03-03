28ddfa452bbbd29ab56214721d2f88fe7bca4267
suminofu0829 <kazuya19840829@yahoo.co.jp>
2019-03-03 20:01:17 +0900

    rails g kaminari:configでkaminari設定ファイルを追加

--- app/controllers/tweet_controller.rb
+++ app/controllers/tweet_controller.rb
@@ -1,10 +1,7 @@
 class TweetController < ApplicationController
-  # PER = 10
 
   def index
     @tweets = Tweet.order(tweet_point: :desc).limit(100).page(params[:page])
-    # @tweets = Tweet.order(tweet_point: :desc).limit(100).page(params[:page]).per(PER)
-
     # 今日から1週間前の日付を取得する
     # Tweetの中で、
   end
