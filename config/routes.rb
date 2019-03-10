53012c485e73d2b0c6abe976302f2cac865b43dc
suminofu0829 <kazuya19840829@yahoo.co.jp>
2019-03-10 15:57:28 +0900

    routingを修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get '/', to: redirect('/tweet/index')
   root 'root#index'
-  get 'root/index'
+  get 'root/index', to: redirect('/users/sign_in')
   get 'tweet/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
