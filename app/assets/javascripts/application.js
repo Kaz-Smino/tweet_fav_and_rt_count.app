e355265c6049e277c3cd6c670cef769f0d3f409d
suminofu0829 <kazuya19840829@yahoo.co.jp>
2019-03-23 18:31:51 +0900

    helperの作成,ページ上部へ移動する動き方の修正

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,3 +19,23 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+
+$(function() {
+  var pagetop = $('#page_top');    
+  pagetop.hide();
+  $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) {  //100pxスクロールしたら表示
+          pagetop.fadeIn();
+      } else {
+          pagetop.fadeOut();
+      }
+  });
+$('a[href^="#"]').click(function(){
+  var time = 500;
+  var href= $(this).attr("href");
+  var target = $(href == "#" ? 'html' : href);
+  var distance = target.offset().top;
+  $("html, body").animate({scrollTop:distance}, time, "swing");
+  return false;
+});
+});
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,32 @@
 module ApplicationHelper
+
+  def minutes_ago
+    @minutes_ago = @seconds_ago / 60
+  end
+
+  def within_1hours?
+    if minutes_ago < 60
+      return true
+    end
+  end
+
+  def hours_ago
+    @hours_ago = @minutes_ago / 60
+  end
+
+  def within_1days?
+    if hours_ago < 60
+      return true
+    end
+  end
+
+  def days_ago
+    @days_ago = @hours_ago / 24
+  end
+
+  def within_1weeks?
+    if @seconds_ago < 60 * 60 * 24 * 8 
+      return true
+    end
+  end
 end
