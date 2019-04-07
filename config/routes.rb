6afa4fe32f49c4a1954a06fe013e56cb64c12db3
suminofu0829 <kazuya19840829@yahoo.co.jp>
2019-04-07 15:15:13 +0900

    mail formを作成

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,9 @@ Rails.application.routes.draw do
 
   root :to => "top#index"
 
+  get 'inquiry' => 'inquiry#index'              # 入力画面
+  post 'inquiry/confirm' => 'inquiry#confirm'   # 確認画面
+  post 'inquiry/thanks' => 'inquiry#thanks'     # 送信完了画面
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
